95f49fc84b321c9c6b421bd58274c251dc5d643f
Gaoridang <oingc@naver.com>
2024-02-02 13:18:34 +0900

    [Remove]: 다음 단계를 위해 코드 삭제
    스타일, 이미지, xml, csv 로더 제거

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import _ from "lodash";
-import "./style.css";
-import Logo from "./logo.png";
-import Data from "./data.xml";
-import Notes from "./data.csv";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], "");
-  element.classList.add("hello");
-
-  const myLogo = new Image();
-  myLogo.src = Logo;
-
-  element.appendChild(myLogo);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 }
