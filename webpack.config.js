167dc5ca34c446f094114136df456e32f9d67303
Gaoridang <oingc@naver.com>
2024-02-02 13:55:32 +0900

    [Implement] 타입스크립트로 마이그레이션

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: {
-    index: "./src/index.js",
-    print: "./src/print.js",
+    index: "./src/index.ts",
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -12,8 +12,25 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  devtool: "eval-source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+    ],
   },
 };
