99ba5f6423e3bc452b2c901a706dfc3ef4a6d279
Lee JaeJun <monooi@naver.com>
2024-02-04 20:50:30 +0900

    [Add] 인풋용 props 추가

--- src/createElement.ts
+++ src/createElement.ts
@@ -1,3 +1,9 @@
+type InputElementProps = {
+  type?: string;
+  value?: string;
+  required?: boolean;
+};
+
 type ElementProps = {
   id: string;
   className: string;
@@ -5,11 +11,12 @@ type ElementProps = {
   attributes: { [key: string]: string };
   children: HTMLElement[] | HTMLElement;
   onClick: (this: HTMLElement, e: MouseEvent) => any;
-};
+  onChange: (this: HTMLElement, e: Event) => any;
+} & InputElementProps;
 
 export default function createElement(
-  tagName: string,
-  props: Partial<ElementProps>
+  tagName: keyof HTMLElementTagNameMap,
+  props?: Partial<ElementProps>
 ) {
   const element = document.createElement(tagName);
 
@@ -18,14 +25,20 @@ export default function createElement(
     if (props.className) element.className = props.className;
     if (props.text) element.textContent = props.text;
     if (props.attributes) {
-      Object.entries(props.attributes).forEach(([key, value]) =>
-        element.setAttribute(key, value)
-      );
+      Object.entries(props.attributes).forEach(([key, value]) => element.setAttribute(key, value));
     }
+
+    if (tagName === "input") {
+      const inputElement = element as HTMLInputElement;
+      if (props.value) inputElement.value = props.value;
+      if (props.type) inputElement.type = props.type;
+      // oninput은 value가 바뀔 즉시 작동
+      if (props.onChange) inputElement.oninput = props.onChange;
+      if (props.required) inputElement.required = props.required;
+    }
+
     if (props.children) {
-      const children = Array.isArray(props.children)
-        ? props.children
-        : [props.children];
+      const children = Array.isArray(props.children) ? props.children : [props.children];
       children.forEach((child) => element.appendChild(child));
     }
     if (props.onClick) {
