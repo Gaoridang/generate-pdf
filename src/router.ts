9715af6c25f77b81272066b598e26609439e3fdd
Lee JaeJun <monooi@naver.com>
2024-02-04 13:26:28 +0900

    [Add] navigate 메소드 추가

--- src/router.ts
+++ src/router.ts
@@ -6,20 +6,25 @@ type Route = Array<{
 export default function createRouter() {
   const routes: Route = [];
 
+  function checkRoutes() {
+    const currentRoute = routes.find(
+      (route) => route.fragment === window.location.hash
+    );
+    currentRoute.component();
+  }
+
   const router = {
     addRoute(fragment: string, component: () => string) {
       routes.push({ fragment, component });
       return this;
     },
 
-    start() {
-      const checkRoutes = () => {
-        const currentRoute = routes.find(
-          (route) => route.fragment === window.location.hash
-        );
-        currentRoute.component();
-      };
+    navigate(fragment: string) {
+      window.location.hash = fragment;
+      checkRoutes();
+    },
 
+    start() {
       window.addEventListener("hashchange", checkRoutes);
       checkRoutes();
     },
