f5f1a01d3fdf19d63aba591518b89c19c1468e63
Gaoridang <oingc@naver.com>
2024-02-04 22:59:28 +0900

    [Add] PDF 생성 API 연결

--- src/createForm.ts
+++ src/createForm.ts
@@ -1,10 +1,11 @@
 import createElement from "./createElement";
+import { createSection } from "./createSection";
 
 const inputTypes = [
   { name: "title", placeholder: "제목을 입력하세요." },
   { name: "position", placeholder: "프론트엔드 개발자" },
-  { name: "phoneNumber", placeholder: "01012345678" },
-  { name: "email", placeholder: "email@example.com" },
+  { name: "phoneNumber", placeholder: "01012345678", type: "tel" },
+  { name: "email", placeholder: "email@example.com", type: "email" },
   { name: "coverLetter", placeholder: "자기소개를 입력하세요." },
   { name: "career", placeholder: "경력을 입력하세요." },
   { name: "projects", placeholder: "외부 활동이나 프로젝트를 입력하세요." },
@@ -15,18 +16,17 @@ export function createForm() {
   const inputs = inputTypes.map((inputType) => {
     const inputEl = createElement("input", {
       className: inputType.name,
-      type: "text",
+      type: inputType.type || "text",
       required: true,
-      attributes: { name: inputType.name, placeholder: inputType.placeholder },
+      attributes: { name: inputType.name, placeholder: inputType.placeholder, autocomplete: "off" },
     });
     return inputEl;
   });
 
-  const button = createElement("button", {
-    className: "submit-button",
-    type: "submit",
-    text: "PDF로 변환하기",
-  });
+  // FIXME: 엔터 누르면 커리어 인풋이 추가됨(?)
+  const careerSection = createSection("career", [inputs[5]]);
+  const projectSection = createSection("project", [inputs[6]]);
+  const urlSection = createSection("url", [inputs[7]]);
 
   const aboutMeContainer = createElement("div", {
     className: "about-me",
@@ -35,29 +35,59 @@ export function createForm() {
 
   const careerContainer = createElement("div", {
     className: "careers",
-    children: [...inputs.slice(5)],
+    children: [careerSection, projectSection, urlSection],
+  });
+
+  const submitButton = createElement("button", {
+    className: "submit-button",
+    type: "submit",
+    text: "PDF로 변환하기",
   });
 
   const form = createElement("form", {
     className: "form",
-    children: [aboutMeContainer, careerContainer, button],
+    children: [aboutMeContainer, careerContainer, submitButton],
     attributes: { action: "#", method: "POST" },
   });
 
-  form.addEventListener("submit", (e) => {
+  form.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const formData = {};
+
+    let formDataHtml = `<div class='form-data'>`;
 
     inputTypes.forEach((inputType) => {
-      const inputEl = document.querySelector(
-        `form input[name=${inputType.name}]`
-      ) as HTMLInputElement;
+      const inputEl = document.querySelector(`input[name="${inputType.name}"]`) as HTMLInputElement;
       if (inputEl) {
-        formData[inputType.name] = inputEl.value;
+        formDataHtml += `
+          <div class=${inputType.name}-container>
+            <h2>${inputType.name}</h2>
+            <p>${inputEl.value}</p>
+          </div>
+        `;
       }
     });
 
-    console.log(formData);
+    formDataHtml += "</div>";
+
+    console.log(formDataHtml);
+
+    try {
+      const response = await fetch("http://localhost:5001/generate-pdf", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ htmlContent: formDataHtml }),
+      });
+
+      if (!response.ok) throw new Error("Failed to generate PDF");
+      // pdf download url
+      const blob = await response.blob();
+      const downloadUrl = URL.createObjectURL(blob);
+      window.open(downloadUrl, "_blank");
+    } catch (error) {
+      console.error("Error generating PDF: ", error);
+    }
   });
 
   return form;
