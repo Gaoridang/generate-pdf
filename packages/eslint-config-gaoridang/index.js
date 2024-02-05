d430f2cbef64613274f0f8022a4ad9af0eba78f7
Lee JaeJun <monooi@naver.com>
2024-02-05 18:06:35 +0900

    [Add] 공통 package 설정

---
+++ packages/eslint-config-gaoridang/index.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: ["airbnb", "airbnb/hooks", "standard-with-typescript", "prettier"],
+  plugins: ["@typescript-eslint"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    semi: ["error", "always"],
+    "no-console": "off",
+    "no-multiple-empty-lines": "error",
+    "@typescript-eslint/no-misused-promises": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+      typescript: {},
+    },
+  },
+};
