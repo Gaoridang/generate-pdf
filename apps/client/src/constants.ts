edf7f667c6cf979841f3154ef5ff4ff64b84039e
Lee JaeJun <monooi@naver.com>
2024-02-05 23:45:25 +0900

    [Update] placeholder 수정

--- apps/client/src/constants.ts
+++ apps/client/src/constants.ts
@@ -4,7 +4,7 @@ export const inputTypes = [
   { kr: "휴대폰 번호", name: "phoneNumber", placeholder: "01012345678", type: "tel" },
   { kr: "이메일", name: "email", placeholder: "email@example.com", type: "email" },
   { kr: "자기 소개", name: "coverLetter", placeholder: "자기소개를 입력하세요." },
-  { kr: "포트폴리오", name: "urls", placeholder: "추가적인 링크를 넣어주세요." },
+  { kr: "포트폴리오", name: "urls", placeholder: "포트폴리오나 기타 링크를 입력하세요." },
 ];
 
 export const textAreaTypes = [
