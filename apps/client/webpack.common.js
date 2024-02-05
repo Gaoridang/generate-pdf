7a6d7865cefcab913738afde6354581e6adc8bb3
Gaoridang <oingc@naver.com>
2024-02-05 18:05:10 +0900

    [Move] 모노레포로 마이그레이션
    apps 폴더로 이동

--- apps/client/webpack.common.js
+++ apps/client/webpack.common.js
@@ -8,14 +8,13 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "가오리당 기술 블로그",
+      title: "이력서 to PDF",
       template: "./index.html",
       meta: {
         author: "gaoridang",
-        Keyowrds: "가오리당",
-        keywords: "html, css, javascript, typescript",
-        description:
-          "웹 개발 기술 블로그입니다! 바닐라 자바스크립트로 개발 중입니다.",
+        Keyowrds: "이력서 PDF",
+        keywords: "이력서, pdf, 개발자이력서",
+        description: "이력서를 원하는 형태의 PDF로 바꿔보세요.",
         robots: "index, follow",
         Date: "2024-02-04T12:14:00+09:00",
       },
